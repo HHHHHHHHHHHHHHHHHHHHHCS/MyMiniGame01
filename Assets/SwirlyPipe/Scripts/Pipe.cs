b36da1a105c24cc398c4f059d01e0c68cb822748
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-07-16 19:50:37 +0800

    add

--- Assets/SwirlyPipe/Scripts/Pipe.cs
+++ Assets/SwirlyPipe/Scripts/Pipe.cs
@@ -14,6 +14,62 @@ public class Pipe : MonoBehaviour
     /// </summary>
     public int curveSegmentCount, pipeSegmentCount;
 
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+    private void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Pipe";
+        SetVertices();
+        SetTriangles();
+        mesh.RecalculateNormals();
+    }
+
+    private void SetVertices()
+    {
+        vertices = new Vector3[pipeSegmentCount * curveSegmentCount * 4];
+        float uStep = (2f * Mathf.PI) / curveSegmentCount;
+        int iDelta = pipeSegmentCount * 4;
+        for (int u = 2, i = iDelta; u <= curveSegmentCount; u++, i += iDelta)
+        {
+            CreateQuadRing(u * uStep, i);
+        }
+
+        mesh.vertices = vertices;
+    }
+
+    private void CreateQuadRing(float u,int i)
+    {
+        float vStep = (2f * Mathf.PI) / pipeSegmentCount;
+        int ringOffset = pipeSegmentCount * 4;
+
+        Vector3 vertex = GetPointOnTorus(u, 0f);
+        //现在用四个点 对应 六个顶点数据
+        for (int v = 1; v <= pipeSegmentCount; v++, i += 4)
+        {
+            vertices[i] = vertices[i - ringOffset + 2];
+            vertices[i + 1] = vertices[i - ringOffset + 3];
+            vertices[i + 2] = vertex;
+            vertices[i + 3] = vertex = GetPointOnTorus(u, v * vStep);
+        }
+    }
+
+    private void SetTriangles()
+    {
+        triangles = new int[pipeSegmentCount * curveSegmentCount * 6];
+        for (int t = 0, i = 0; t < triangles.Length; t += 6, i += 4)
+        {
+            triangles[t] = i;
+            triangles[t + 1] = triangles[t + 4] = i + 1;
+            triangles[t + 2] = triangles[t + 3] = i + 2;
+            triangles[t + 5] = i + 3;
+        }
+
+        mesh.triangles = triangles;
+    }
+
 
     private Vector3 GetPointOnTorus(float u, float v)
     {
@@ -40,7 +96,6 @@ public class Pipe : MonoBehaviour
                     (float) v / pipeSegmentCount,
                     (float) u / curveSegmentCount);
                 Gizmos.DrawSphere(point, 0.1f);
-                
             }
         }
     }
